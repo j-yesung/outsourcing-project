33989f0b93dea996b9ff842efba85e81280d194b
j-yesung <hisung-ah@kakao.com>
2023-12-07 09:50:19 +0900

    [예성] 크롤링 작업 중

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -13,6 +13,13 @@ const GlobalStyle = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
+  @font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
   body {
     font-family: 'Pretendard-Regular';
   }
