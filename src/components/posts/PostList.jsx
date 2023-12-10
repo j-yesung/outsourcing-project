15539b0b7fef7e379c42f80ee9348ab07c6e6b98
lsw502 <tkddnr1216@naver.com>
2023-12-11 01:26:02 +0900

    [상욱]스크롤 탑 구현

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -5,29 +5,33 @@ import * as S from '../../styles/posts/PostList.styled';
 import { getFormattedDate } from 'utils/date';
 import defaultImage from '../../assets/user.svg';
 
+import ScrollToTopBtn from './ScrollToTopBtn';
 const PostList = () => {
   const { posts } = usePosts();
   const navigate = useNavigate();
 
   return (
-    <S.PostContainer>
-      <S.PostWrapper>
-        {posts?.map((item, idx) => {
-          return (
-            <S.PostList key={idx} onClick={() => navigate(`/post/${item.id}`)}>
-              <S.PostTitleContentsDate>
-                <S.PostTitle>{item.title}</S.PostTitle>
-                <S.PostContent>{item.contents}</S.PostContent>
-                <S.PostDate>{getFormattedDate(item.createdAt)}</S.PostDate>
-              </S.PostTitleContentsDate>
-              <S.Postimge>
-                <img src={defaultImage} />
-              </S.Postimge>
-            </S.PostList>
-          );
-        })}
-      </S.PostWrapper>
-    </S.PostContainer>
+    <>
+      <S.PostContainer>
+        <S.PostWrapper>
+          {posts?.map((item, idx) => {
+            return (
+              <S.PostList key={idx} onClick={() => navigate(`/post/${item.id}`)}>
+                <S.PostTitleContentsDate>
+                  <S.PostTitle>{item.title}</S.PostTitle>
+                  <S.PostContent>{item.contents}</S.PostContent>
+                  <S.PostDate>{getFormattedDate(item.createdAt)}</S.PostDate>
+                </S.PostTitleContentsDate>
+                <S.Postimge>
+                  <img src={defaultImage} />
+                </S.Postimge>
+              </S.PostList>
+            );
+          })}
+        </S.PostWrapper>
+      </S.PostContainer>
+      <ScrollToTopBtn />
+    </>
   );
 };
 
