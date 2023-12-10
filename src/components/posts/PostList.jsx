b31237f936a20c520717904c5cd915829f35b701
yeonna18k <yeonna18k@gmail.com>
2023-12-10 23:47:04 +0900

    [나연] 포스트 작성, 포스트목록 UI

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { usePosts } from 'hooks/usePosts';
 import { useNavigate } from 'react-router-dom';
-import * as S from '../../styles/posts/PostContent.styled';
+import * as S from '../../styles/posts/PostList.styled';
 import { getFormattedDate } from 'utils/date';
+import defaultImage from '../../assets/user.svg';
 
 const PostList = () => {
   const { posts } = usePosts();
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div> ⬇️ post불러와서 map으로 뿌려주기 ⬇️ </div>
-      {posts?.map((item, idx) => {
-        return (
-          <S.TempDiv key={idx} onClick={() => navigate(`/post/${item.id}`)}>
-            <div>제목 : {item.title}</div>
-            <span>날짜 : {getFormattedDate(item.createdAt)}</span>
-            <div>내용 : {item.contents}</div>
-            <div>게시글 ID : {item.id}</div>
-            <div>게시글 uid : {item.uid}</div>
-          </S.TempDiv>
-        );
-      })}
-    </div>
+    <S.PostContainer>
+      <S.PostWrapper>
+        {posts?.map((item, idx) => {
+          return (
+            <S.PostList key={idx} onClick={() => navigate(`/post/${item.id}`)}>
+              <S.PostTitleContentsDate>
+                <S.PostTitle>{item.title}</S.PostTitle>
+                <S.PostContent>{item.contents}</S.PostContent>
+                <S.PostDate>{getFormattedDate(item.createdAt)}</S.PostDate>
+              </S.PostTitleContentsDate>
+              <S.Postimge>
+                <img src={defaultImage} />
+              </S.Postimge>
+            </S.PostList>
+          );
+        })}
+      </S.PostWrapper>
+    </S.PostContainer>
   );
 };
 
