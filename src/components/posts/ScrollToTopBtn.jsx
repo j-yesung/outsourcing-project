e353206e961f33ab045619c0b5a32656b1232b5f
j-yesung <hisung-ah@kakao.com>
2023-12-11 02:41:59 +0900

    스크롤 일정 영역 벗어나면 TOP 버튼 생성

--- src/components/posts/ScrollToTopBtn.jsx
+++ src/components/posts/ScrollToTopBtn.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 function ScrollToTopBtn() {
-  const handleClick = () => {
-    if (!window.scrollY) return;
-    // 현재 위치가 이미 최상단일 경우 return
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+  const scrollToTopBtnRef = useRef(null);
+  const handleScroll = () => {
+    if (!scrollToTopBtnRef.current) return;
+    if (window.scrollY > 150) {
+      scrollToTopBtnRef.current.style.scale = '1';
+    } else {
+      scrollToTopBtnRef.current.style.scale = '0';
+    }
   };
-  return <Container onClick={handleClick}>TOP</Container>;
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const handleScrollToTop = useCallback(() => {
+    if (scrollToTopBtnRef.current) {
+      document.documentElement.scrollTop = 0;
+    }
+  }, []);
+  return (
+    <StScrollToTopBtn ref={scrollToTopBtnRef} onClick={handleScrollToTop}>
+      TOP
+    </StScrollToTopBtn>
+  );
 }
-
 export default ScrollToTopBtn;
-
-const Container = styled.button`
+const StScrollToTopBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
-  top: 50%;
+  bottom: 20px;
   right: 30px;
   width: 60px;
   height: 60px;
-  transform: translateY(300px);
-  background-color: #e31c5f;
+  padding: 10px;
   border-radius: 50%;
+  border: none;
+  color: #fff;
+  background-color: #e31c5f;
+  box-shadow: var(--box-shadow);
+  transition: 0.2s ease-in-out;
+  scale: 0;
+  cursor: pointer;
+  &:hover {
+    scale: 1.2;
+  }
+  border: 5px solid white;
   font-size: 14px;
   font-weight: bold;
-  letter-spacing: -0.28px;
-  text-align: center;
-  color: #fff;
-  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.16);
-  cursor: pointer;
-  z-index: 99999;
-  border: 5px solid white;
 `;
