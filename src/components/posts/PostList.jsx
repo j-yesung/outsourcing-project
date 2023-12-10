9ec119daccd9a01b083fd049d2ed3135af23681e
lsw502 <tkddnr1216@naver.com>
2023-12-10 23:55:23 +0900

    [상욱]테스트

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -3,6 +3,7 @@ import { usePosts } from 'hooks/usePosts';
 import { useNavigate } from 'react-router-dom';
 import * as S from '../../styles/posts/PostContent.styled';
 import { getFormattedDate } from 'utils/date';
+import ScrollTop from './ScrollTop';
 
 const PostList = () => {
   const { posts } = usePosts();
@@ -13,13 +14,16 @@ const PostList = () => {
       <div> ⬇️ post불러와서 map으로 뿌려주기 ⬇️ </div>
       {posts?.map((item, idx) => {
         return (
-          <S.TempDiv key={idx} onClick={() => navigate(`/post/${item.id}`)}>
-            <div>제목 : {item.title}</div>
-            <span>날짜 : {getFormattedDate(item.createdAt)}</span>
-            <div>내용 : {item.contents}</div>
-            <div>게시글 ID : {item.id}</div>
-            <div>게시글 uid : {item.uid}</div>
-          </S.TempDiv>
+          <>
+            <S.TempDiv key={idx} onClick={() => navigate(`/post/${item.id}`)}>
+              <div>제목 : {item.title}</div>
+              <span>날짜 : {getFormattedDate(item.createdAt)}</span>
+              <div>내용 : {item.contents}</div>
+              <div>게시글 ID : {item.id}</div>
+              <div>게시글 uid : {item.uid}</div>
+            </S.TempDiv>
+            <ScrollTop />
+          </>
         );
       })}
     </div>
---
+++ src/components/posts/ScrollTop.jsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+function ScrollTop() {
+  const scrollToTopBtnRef = useRef(null);
+  const handleScroll = () => {
+    if (!scrollToTopBtnRef.current) return;
+    if (window.scrollY > 150) {
+      scrollToTopBtnRef.current.style.scale = '1';
+    } else {
+      scrollToTopBtnRef.current.style.scale = '0';
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const handleScrollToTop = useCallback(() => {
+    if (scrollToTopBtnRef.current) {
+      document.documentElement.scrollTop = 0;
+    }
+  }, []);
+  return (
+    <StScrollToTopBtn ref={scrollToTopBtnRef} onClick={handleScrollToTop}>
+      ▲
+    </StScrollToTopBtn>
+  );
+}
+export default ScrollTop;
+const StScrollToTopBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+  border-radius: 50%;
+  border: none;
+  color: #fff;
+  background-color: #ff5b22;
+  box-shadow: var(--box-shadow);
+  transition: 0.2s ease-in-out;
+  scale: 0;
+  cursor: pointer;
+  &:hover {
+    scale: 1.2;
+  }
+`;
